8ff6067829a58e7b7c1885ff826c9e0441ed08dd
Steven Harradine <stevenharradine@users.noreply.github.com>
2017-11-18 14:32:58 -0500

    add more configurables, move to wget

--- installer.sh
+++ installer.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 # (c) 2017 Steven Harradine
-installPath=/usr/local/bin
+installDir=/usr/local/bin
+githubUser=stevenharradine
+gitBranch=master
 
 if [ "$EUID" -ne 0 ]; then
-	echo "Please run as root"
+        echo "Please run as root"
 else
-	for arg in "$@"; do
-		key=`echo "$arg" | awk -F "=" '{print $1}'`
-		value=`echo "$arg" | awk -F "=" '{print $2}'`
+        for arg in "$@"; do
+                key=`echo "$arg" | awk -F "=" '{print $1}'`
+                value=`echo "$arg" | awk -F "=" '{print $2}'`
 
-		if [[ $key == installPath ]]; then
-			installPath=$value
-		elif [[ $key == program ]]; then
-			program=$value
-		fi
-	done
+                if [[ $key == "installDir" ]]; then
+                        installDir=$value
+                elif [[ $key == "program" ]]; then
+                        program=$value
+                elif [[ $key == "installPath" ]]; then
+                        installPath=$value
+                elif [[ $key == "githubUser" ]]; then
+                        githubUser=$value
+                elif [[ $key == "gitBranch" ]]; then
+                        gitBranch=$value
+                fi
+        done
 
-	if [[ $program == "" ]]; then
-		echo "You must define a program when calling the installer"
-	else
-		echo -n "Installing . "
-		cp $program.sh $installPath/$program
-		chown root:root $installPath/$program
-		chmod 755 $installPath/$program
-		echo "Done"
-	fi
+        installPath=$installDir/$program
+
+        if [[ $program == "" ]]; then
+                echo "You must define a program when calling the installer"
+        else
+                echo -n "Installing . "
+                wget --quiet --output-document $installPath https://raw.githubusercontent.com/$githubUser/$program/$gitBranch/$program.sh
+                chown root:root $installPath
+                chmod 755 $installPath
+                echo "Done"
+        fi
 fi
